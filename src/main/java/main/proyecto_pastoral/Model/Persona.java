390186e65c933e714d2877d1461536d4057352ac
FE-csc <felipe.arayarivera@hotmail.com>
2026-04-07 15:33:30 -0600

    Fix: corrección de errores en las queries

--- src/main/java/main/proyecto_pastoral/Model/Persona.java
+++ src/main/java/main/proyecto_pastoral/Model/Persona.java
@@ -1,15 +1,27 @@
 package main.proyecto_pastoral.Model;
 
 import jakarta.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "Persona")
 @NamedQueries(value = {
-        @NamedQuery(name = "Persona.buscarConIngresosPorRegistro",query = "SELECT p FROM Persona p LEFT JOIN FETCH p.ingresos WHERE p.registro.numeroRegistro = :numero"),
-        @NamedQuery(name = "Persona.buscarJefeFamilia", query = "SELECT p FROM Persona p WHERE p.registro = :numero AND p.jefatura = 'SI'"),
-        @NamedQuery(name = "Persona.buscarPorRegistro", query = "SELECT p FROM Persona p WHERE P.registro = :numero ORDER BY p.jefatura DESC")
+
+        @NamedQuery(
+                name = "Persona.buscarConIngresosPorRegistro",
+                query = "SELECT p FROM Persona p LEFT JOIN FETCH p.ingresos WHERE p.registro.numeroRegistro = :numero"
+        ),
+        @NamedQuery(
+                name = "Persona.buscarJefeFamilia",
+                query = "SELECT p FROM Persona p WHERE p.registro.numeroRegistro = :numero AND p.jefatura = 'SI'"
+        ),
+
+        @NamedQuery(
+                name = "Persona.buscarPorRegistro",
+                query = "SELECT p FROM Persona p WHERE p.registro.numeroRegistro = :numero ORDER BY p.jefatura DESC"
+        )
 })
 public class Persona {
     @Id
@@ -69,17 +81,18 @@ public class Persona {
     @OneToMany(mappedBy = "persona", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<IngresoFamiliar> ingresos = new ArrayList<>();
 
-    public Persona() {}
+    public Persona() {
+    }
 
     public Persona(String tipoDocumento, String numeroIdentificacion,
-                   String nombre,String primerApellido,String segundoApellido,String telefono,String sexo, String jefatura, String relacion, int edad,
+                   String nombre, String primerApellido, String segundoApellido, String telefono, String sexo, String jefatura, String relacion, int edad,
                    String pais, String migracion, String educacion,
                    String salud, String seguro, Registro registro) {
         this.tipoDocumento = tipoDocumento;
         this.numeroIdentificacion = numeroIdentificacion;
         this.nombre = nombre;
         this.PrimerApellido = primerApellido;
-        this.SegundoApellido =segundoApellido;
+        this.SegundoApellido = segundoApellido;
         this.telefono = telefono;
         this.sexo = sexo;
         this.jefatura = jefatura;
@@ -93,50 +106,125 @@ public class Persona {
         this.registro = registro;
     }
 
-    public int getIdPersona() { return idPersona; }
-    public void setIdPersona(int idPersona) { this.idPersona = idPersona; }
+    public int getIdPersona() {
+        return idPersona;
+    }
 
-    public String getTipoDocumento() { return tipoDocumento; }
-    public void setTipoDocumento(String tipoDocumento) { this.tipoDocumento = tipoDocumento; }
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
 
-    public String getNumeroIdentificacion() { return numeroIdentificacion; }
-    public void setNumeroIdentificacion(String numeroIdentificacion) { this.numeroIdentificacion = numeroIdentificacion; }
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
 
-    public String getSexo() { return sexo; }
-    public void setSexo(String sexo) { this.sexo = sexo; }
+    public String getNumeroIdentificacion() {
+        return numeroIdentificacion;
+    }
 
-    public String isJefatura() { return jefatura; }
-    public void setJefatura(String jefatura) { this.jefatura = jefatura; }
+    public void setNumeroIdentificacion(String numeroIdentificacion) {
+        this.numeroIdentificacion = numeroIdentificacion;
+    }
 
-    public String getRelacion() { return relacion; }
-    public void setRelacion(String relacion) { this.relacion = relacion; }
+    public String getNombre() {
+        return nombre;
+    }
 
-    public int getEdad() { return edad; }
-    public void setEdad(int edad) { this.edad = edad; }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-    public String getPais() { return pais; }
-    public void setPais(String pais) { this.pais = pais; }
+    public String getSexo() {
+        return sexo;
+    }
 
-    public String getMigracion() { return migracion; }
-    public void setMigracion(String migracion) { this.migracion = migracion; }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
 
-    public String getEducacion() { return educacion; }
-    public void setEducacion(String educacion) { this.educacion = educacion; }
+    public String isJefatura() {
+        return jefatura;
+    }
 
-    public String getSalud() { return salud; }
-    public void setSalud(String salud) { this.salud = salud; }
+    public void setJefatura(String jefatura) {
+        this.jefatura = jefatura;
+    }
 
-    public String getSeguro() { return seguro; }
-    public void setSeguro(String seguro) { this.seguro = seguro; }
+    public String getRelacion() {
+        return relacion;
+    }
 
-    public Registro getRegistro() { return registro; }
-    public void setRegistro(Registro registro) { this.registro = registro; }
+    public void setRelacion(String relacion) {
+        this.relacion = relacion;
+    }
 
-    public List<IngresoFamiliar> getIngresos() { return ingresos; }
-    public void setIngresos(List<IngresoFamiliar> ingresos) { this.ingresos = ingresos; }
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getMigracion() {
+        return migracion;
+    }
+
+    public void setMigracion(String migracion) {
+        this.migracion = migracion;
+    }
+
+    public String getEducacion() {
+        return educacion;
+    }
+
+    public void setEducacion(String educacion) {
+        this.educacion = educacion;
+    }
+
+    public String getSalud() {
+        return salud;
+    }
+
+    public void setSalud(String salud) {
+        this.salud = salud;
+    }
+
+    public String getSeguro() {
+        return seguro;
+    }
+
+    public void setSeguro(String seguro) {
+        this.seguro = seguro;
+    }
+
+    public Registro getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(Registro registro) {
+        this.registro = registro;
+    }
+
+    public List<IngresoFamiliar> getIngresos() {
+        return ingresos;
+    }
+
+    public void setIngresos(List<IngresoFamiliar> ingresos) {
+        this.ingresos = ingresos;
+    }
 
     public String getSegundoApellido() {
         return SegundoApellido;
@@ -157,11 +245,12 @@ public class Persona {
     public String getTelefono() {
         return telefono;
     }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
-    public String getNombreCompleto(){
+    public String getNombreCompleto() {
         return nombre + " " + PrimerApellido + " " + SegundoApellido;
     }
 
